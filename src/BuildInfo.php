4ae420f22b7551c7c06b1f68dda668810f91c3be
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2026-08-31 18:56:03 -0500

    Prepare CLI 2.0.0 stable (#27)
    Co-authored-by: Durable Workflow <support@durable-workflow.com>

--- src/BuildInfo.php
+++ src/BuildInfo.php
@@ -6,7 +6,7 @@ namespace DurableWorkflow\Cli;
 
 final class BuildInfo
 {
-    private const FALLBACK_VERSION = '2.0.0-rc.36';
+    private const FALLBACK_VERSION = '2.0.0';
 
     public static function consoleVersion(): string
     {
